b464bb200cf44246cb2da8b202ab8ae69ad8d9d2
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2023-03-11 00:53:28 +0100

    Make json serialization less strict
    - Unknown keys are now allowed.
    - Quoted boolean literals, and unquoted string literals are now allowed.

--- src/main/kotlin/de/joshi/modpackdownloader/parser/ManifestParser.kt
+++ src/main/kotlin/de/joshi/modpackdownloader/parser/ManifestParser.kt
@@ -13,13 +13,18 @@ import java.nio.file.Files
 class ManifestParser {
     private val LOGGER = KotlinLogging.logger { }
 
+    private val json = Json {
+        isLenient = true
+        ignoreUnknownKeys = true
+    }
+
     fun getManifest(modpackFile: File): ManifestData {
 
         val manifestFile = modpackFile.getSubfolder("manifest.json")
             ?: throw FileNotFoundException("No manifest.json file could be found in $modpackFile")
 
         LOGGER.info("Reading Manifest File from $manifestFile")
-        val manifestData = Json.decodeFromString<ManifestData>(Files.readString(manifestFile.toPath()))
+        val manifestData = json.decodeFromString<ManifestData>(Files.readString(manifestFile.toPath()))
         LOGGER.info("Manifest Info:")
         LOGGER.info("Modpack: ${manifestData.name} ${manifestData.version} by ${manifestData.author.ifBlank { "an unknown author" }}")
         LOGGER.info("Minecraft Version: ${manifestData.minecraft.version}, ModLoader: ${manifestData.minecraft.modLoaders.filter { it.primary }.getOrElse(0) { ModLoaderData("unknown", true) }.id}")
