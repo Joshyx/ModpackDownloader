83c4eadc7f9f53428fbc20b0b4d7b1f08c8a8582
Joshi <joshua_boehme@t-online.de>
2024-01-02 15:25:11 +0100

    Removes tests as key is not necessary

--- src/test/kotlin/de/joshi/modpackdownloader/auth/CurseforgeApiKeyTest.kt
+++ src/test/kotlin/de/joshi/modpackdownloader/auth/CurseforgeApiKeyTest.kt
@@ -1,22 +1,11 @@
 package de.joshi.modpackdownloader.auth
 
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
 class CurseforgeApiKeyTest {
     @Test
     fun isApiKeyValidReturnsFalse() {
         assertFalse(CurseforgeApiKey.isApiKeyValid("hallo"))
-    }
-
-    @Test
-    fun getApiKeyReturnsNotNull() {
-        assertNotNull(CurseforgeApiKey.getApiKey())
-    }
-
-    @Test
-    fun hasValidApiKeyReturnsTrue() {
-        assert(CurseforgeApiKey.hasValidApiKey())
     }
 }
